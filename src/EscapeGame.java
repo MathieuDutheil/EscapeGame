b1a8b07b590654f31061aab4227964e4c0210bdd
Mathieu Dutheil <mathieudutheil92@gmail.com>
2020-02-08 23:19:24 +0100

    add EscapeGame class     add runGame method     add 3 startMode method
    add Utilities class
        add askAnInt method
        add getRandomNumberInRange method

--- src/EscapeGame.java
+++ src/EscapeGame.java
@@ -1,13 +1,39 @@
 public class EscapeGame {
-
+    private int gameMode;
+    private int secretCombination;
 
     public void selectGameMode() {
+        gameMode = Utilities.askAnInt("À quels modes du jeu souhaitez-vous jouer ? (1 : Challenger, 2 : Défenseur, 3 : Duel)", 1, 3);
+    }
+
+    public void startChallengerMode() {
+    gameMode = Utilities.getRandomNumberInRange(0,9999);
+        System.out.println(gameMode);
+    }
+
+    public void startDefenderMode() {
 
     }
 
+    public void startDualMode() {
+
+    }
 
     public void runGame() {
+        System.out.println("Bienvenue dans Escape Game ONLINE");
         selectGameMode();
+        switch (gameMode) {
+            case 1:
+                startChallengerMode();
+                break;
+            case 2:
+                startDefenderMode();
+                break;
+            default:
+                startDualMode();
+                break;
+
+        }
 
     }
 }
---
+++ src/Utilities.java
@@ -0,0 +1,40 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Utilities {
+
+    private static Scanner sc = new Scanner(System.in);
+
+
+    public static int askAnInt(String intro, int lowLimit, int highLimit) {
+
+        int carac = 0;
+        boolean isNonNumericalOutput;
+        do {
+            try {
+                isNonNumericalOutput = false;
+                System.out.println(intro);
+                carac = sc.nextInt();
+                if (carac < lowLimit || carac > highLimit) {
+                    System.out.println("Attention la saisie attendue doit être comprise entre " + lowLimit + " et " + highLimit + ", recommencez la saisie.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Merci de saisir un chiffre.");
+                isNonNumericalOutput = true;
+                sc.nextLine();
+            }
+        } while (carac < lowLimit || carac > highLimit || isNonNumericalOutput);
+        return carac;
+    }
+
+    public static int getRandomNumberInRange(int min, int max) {
+
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        return (int) (Math.random() * ((max - min) + 1)) + min;
+    }
+}
+
