b80c3af9384f48fc0b145b6f0b9a0a589796a660
Mathieu Dutheil <mathieudutheil92@gmail.com>
2020-04-01 21:36:16 +0200

    review the local scope of game

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,22 @@ public class Main {
         do {
             int gameMode = Utilities.askAnInt("À quels modes du jeu souhaitez-vous jouer ? (1 : Challenger, 2 : Défenseur, 3 : Duel)", 1, 3);
             do {
+                AbstractGame game;
                 switch (gameMode) {
                     case 1:
-                        AbstractGame game = new ChallengerMode();
-                        game.runGame();
+                        game = new ChallengerMode();
+
                         break;
                     case 2:
                         game = new DefenderMode();
-                        game.runGame();
+
                         break;
                     default:
                         game = new DualMode();
-                        game.runGame();
+
                         break;
                 }
+                game.runGame();
 
                 restart = Utilities.askAnInt("Voulez-vous : 1 - Rejouer au même mode, 2 - Lancer un autre mode, 3 - Quitter l'application.", 1, 3);
             }
