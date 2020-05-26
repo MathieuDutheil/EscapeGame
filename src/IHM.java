312110a2d806a868177e7a88f1485a81f98b5c05
Mathieu Dutheil <mathieudutheil92@gmail.com>
2020-05-26 12:35:54 +0200

    create new branch and begin to implement IHM class and EscapeGameApp class

--- src/IHM.java
+++ src/IHM.java
@@ -1,3 +1,35 @@
-public class IHM {
+import java.util.Scanner;
 
+public class IHM {
+    private static Scanner sc = new Scanner(System.in);
+
+    public IHM() {
+        runIHM();
+    }
+
+    public void runIHM() {
+        int restart;
+        do {
+            int gameMode = Utilities.askAnInt("À quels modes du jeu souhaitez-vous jouer ? (1 : Challenger, 2 : Défenseur, 3 : Duel)", 0, 3);
+            do {
+
+                AbstractGame game;
+                switch (gameMode) {
+                    case 1:
+                        game = new ChallengerMode();
+                        break;
+                    case 2:
+                        game = new DefenderMode();
+                        break;
+                    default:
+                        game = new DualMode();
+                        break;
+                }
+                game.runGame();
+
+                restart = Utilities.askAnInt("Voulez-vous : 1 - Rejouer au même mode, 2 - Lancer un autre mode, 3 - Quitter l'application.", 1, 3);
+            }
+            while (restart == 1);
+        } while (restart == 2);
+    }
 }
