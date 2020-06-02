935771d0a9878eee1a43f69a43f0ea35fb405e7d
Mathieu Dutheil <mathieudutheil92@gmail.com>
2020-06-02 21:30:25 +0200

    some Trash code who will be deleted when i have finished to implement IHM

---
+++ src/Trash.java
@@ -0,0 +1,35 @@
+public class Trash {
+    /* TRASH TRASH TRASH TRASH TRASH
+    String compareGuessWithCombination(String guess, String secret, String playerName) {
+        LOGGER.trace("method compareGuessWithCombination started");
+        LOGGER.debug("guess = " + guess + ", secret = " + secret);
+        String clue = "";
+        int nbGoodResponses = 0;
+        for (int i = 0; i < lengthCombination; i++) {
+
+            if (guess.charAt(i) < secret.charAt(i)) {
+                clue += "+";
+
+            } else if (guess.charAt(i) > secret.charAt(i)) {
+                clue += "-";
+
+            } else {
+                clue += "=";
+                nbGoodResponses++;
+            }
+        }
+        LOGGER.debug("clue = " + clue);
+        LOGGER.debug("nbGoodResponses = " + nbGoodResponses);
+        System.out.println(clue);
+        if (nbGoodResponses == lengthCombination) {
+            System.out.println("Bravo " + playerName + " a découvert la combinaison de son adversaire.");
+            partyWon = true;
+            LOGGER.debug("partyWon = " + partyWon);
+        }
+
+        LOGGER.trace("method compareGuessWithCombination finished");
+        return clue;
+
+    }
+    */
+}
