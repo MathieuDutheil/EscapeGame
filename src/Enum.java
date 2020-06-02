71595bda133caaa0b871a850ea03bc3ed0fc0123
Mathieu Dutheil <mathieudutheil92@gmail.com>
2020-06-02 18:39:01 +0200

    put all Enum in a class named Enum

---
+++ src/Enum.java
@@ -0,0 +1,14 @@
+public class Enum {
+
+    public enum Players {
+        PLAYER, COMPUTER
+    }
+
+    public enum StateOfTheGame {
+        START, RUN, END, CLOSE_GAME
+    }
+
+    public enum WhatIsAsked {
+        COMBINATION, NULL
+    }
+}
