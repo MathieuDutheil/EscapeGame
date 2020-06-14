aa166ff26cdbc363a69795c20260dc3ad738c2df
Mathieu Dutheil <mathieudutheil92@gmail.com>
2020-06-14 16:39:03 +0200

    delete side branch

--- src/EscapeGameApp.java
+++ src/EscapeGameApp.java
@@ -4,7 +4,7 @@ public class EscapeGameApp {
 
     private static final org.apache.log4j.Logger LOGGER = Logger.getLogger(EscapeGameApp.class);
 
-    public static void main(String[] args) throws CombinationIncorrectException {
+    public static void main(String[] args) {
         LOGGER.info("App started");
 
         IHM ihm = new IHM();
